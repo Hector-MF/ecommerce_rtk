961906c11ab70e2cfd13efcd3812c54539ae1d61
Hector Marinez <hmartinez1202@gmail.com>
2024-12-24 18:42:18 +0100

    Steps 3 & 4 Actions and Reducers creation

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
 
 const CartSlice = ({
-    cartItems: [],
+
 });
 
+const initialState = {
+  cartItems: [],
+};
 
+const CartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+  }
+});
